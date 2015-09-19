f9a71f23a4e0f60ebd5fb4aee90f08df5e056a19
Taranjeet <reachtotj@gmail.com>
2015-09-19 12:05:42 +0530

    suggestion page fixed

--- gaanabhej/playlist/views.py
+++ gaanabhej/playlist/views.py
@@ -1,4 +1,4 @@
-import urllib2,json
+import requests,json
 from lxml import etree
 
 from django.shortcuts import render
@@ -6,6 +6,8 @@ from django.views.generic import ListView
 from django.contrib.auth.models import User
 from django.db.models import Q
 from django.http import HttpResponse,HttpResponseRedirect
+from django.template.context_processors import csrf
+from django.utils.safestring import mark_safe
 
 from .forms import PlayListForm
 from playlist.models import SongDetails,SuggestedSongs
@@ -17,61 +19,138 @@ def convertToInt(no):
 
 class SuggestASong(ListView):
 
-	template_name		= 'suggestasong.html'
+	template_name		= 'newSuggestion.html'
 
 
 	def get(self,request,*args,**kwargs):
 
 		formObj			= PlayListForm(request.GET,user=request.user)
-		suggestionToBeViewed = None
-		# userList		= User.objects.filter(~Q(id=request.user.id))
-		# selectUserList  = ()
-		# for eachUser in userList:
-			# print eachUser.id,eachUser.username
-			# selectUserList = selectUserList + (eachUser.id,eachUser.username)
-		# print selectUserList
-		# formObj.fields["suggestedTo"].initial = selectUserList
-		# print userList 
+		# suggestionToBeViewed = None
+		# parseError = False
+		# alertMsg, alrtBstrpCls = [None]*2
+		
+		# suggestedBy = request.user
 
-		# suggestedTo = User.objects.get(id=2)
-		suggestedBy = request.user
+		# if formObj.is_valid():
 
-		if formObj.is_valid():
+		# 	formCleaned 		= formObj.cleaned_data
+		# 	songURL				= formCleaned.get("songName",None)
+		# 	suggestedToId 		= formCleaned.get("suggestedTo",None)
+		# 	suggestedTo 		= User.objects.get(id=suggestedToId)
+		# 	# print suggestedBy,suggestedTo
 
-			formCleaned 		= formObj.cleaned_data
-			songURL				= formCleaned.get("songName",None)
-			suggestedToId 		= formCleaned.get("suggestedTo",None)
-			suggestedTo 		= User.objects.get(id=suggestedToId)
-			print suggestedBy,suggestedTo
+		# 	if songURL is not None:
+		# 		print songURL
+		# 		try:
+		# 			page			= requests.get(songURL).text
+		# 			x	 			= etree.HTML(page)
+		# 			title			= x.xpath('//title/text()')[0]
+		# 			views			= convertToInt(x.xpath('//div[@class="watch-view-count"]/text()')[0])
+		# 			likes			= convertToInt(x.xpath('//button[@title="Unlike"]/span[@class="yt-uix-button-content"]/text()')[0])
+		# 			dislikes		= convertToInt(x.xpath('//button[@title="I dislike this"]/span[@class="yt-uix-button-content"]/text()')[0])
+		# 		except Exception,e:
+		# 			print 'Error : Parsing song info ',e
+		# 			title,views,likes,dislikes = [None]*4
+		# 			parseError = True
+		# 		# duration 		= x.xpath('//span[@class="video-time"]/text()')
+		# 		# print title,views,likes,dislikes
+		# 		if not parseError:
+		# 			newsong = SongDetails(
+		# 				url=songURL,name=title,views=views,
+		# 				dislikes=dislikes,likes=likes
+		# 				)
+		# 			newsong.save()
+		# 			suggestion = SuggestedSongs(suggestedTo=suggestedTo,
+		# 				suggestedBy=suggestedBy,song=newsong,isSeen=False)
+		# 			suggestion.save()
 
-			if songURL is not None:
-				print songURL
-				page			= urllib2.urlopen(songURL).read()
-				x	 			= etree.HTML(page)
-				title			= x.xpath('//title/text()')[0]
-				views			= convertToInt(x.xpath('//div[@class="watch-view-count"]/text()')[0])
-				likes			= convertToInt(x.xpath('//button[@title="Unlike"]/span[@class="yt-uix-button-content"]/text()')[0])
-				dislikes		= convertToInt(x.xpath('//button[@title="I dislike this"]/span[@class="yt-uix-button-content"]/text()')[0])
-				# duration 		= x.xpath('//span[@class="video-time"]/text()')
-				print title,views,likes,dislikes,len(title)
-				newsong = SongDetails(
-					url=songURL,name=title,views=views,
-					dislikes=dislikes,likes=likes
-					)
-				newsong.save()
+		# 			alertMsg 	= mark_safe('''Your song is successfully suggested to %s. 
+		# 						By the ways it was a %s song''' % (
+		# 							suggestedTo.username,
+		# 							'<i class="glyphicon glyphicon-heart"></i>'
+		# 							))
+		# 			alrtBstrpCls = 'info'
 
-				
 
-				suggestion = SuggestedSongs(suggestedTo=suggestedTo,
-					suggestedBy=suggestedBy,song=newsong,isSeen=False)
-				suggestion.save()
+
+		# 		else:
+		# 			alertMsg	 = '''We are facing some error in retriving  
+		# 							media info. Can you please try again later'''
+		# 			alrtBstrpCls  = 'danger'
 
 		# suggestionToBeViewed = SuggestedSongs.objects.filter(suggestedTo=suggestedBy.id,isSeen=False)
 		# print suggestionToBeViewed
 		return render(request,self.template_name,{
 			'form'			: formObj,
+			# 'alrtBstrpCls'	: alrtBstrpCls,
+			# 'alertMsg'		: alertMsg
 
 			})
+
+	def post(self,request,*args,**kwargs):
+
+		# formObj					= PlayListForm(request.GET,user=request.user)
+		suggestionToBeViewed 	= None
+		parseError 				= False
+		alertMsg, alrtBstrpCls 	= [None]*2
+		d 						= {}
+		
+
+		if request.user.is_authenticated():
+			suggestedBy			= request.user
+			songURL				= request.POST.get("songName",None)
+			suggestedToId 		= request.POST.get("suggestedTo",None)
+			suggestedTo 		= User.objects.get(id=suggestedToId)
+
+			if songURL is not None:
+				# print songURL
+				try:
+					page			= requests.get(songURL).text
+					x	 			= etree.HTML(page)
+					title			= x.xpath('//title/text()')[0]
+					views			= convertToInt(x.xpath('//div[@class="watch-view-count"]/text()')[0])
+					likes			= convertToInt(x.xpath('//button[@title="Unlike"]/span[@class="yt-uix-button-content"]/text()')[0])
+					dislikes		= convertToInt(x.xpath('//button[@title="I dislike this"]/span[@class="yt-uix-button-content"]/text()')[0])
+				except Exception,e:
+					print 'Error : Parsing song info ',e
+					title,views,likes,dislikes = [None]*4
+					parseError = True
+				# duration 		= x.xpath('//span[@class="video-time"]/text()')
+				# print title,views,likes,dislikes
+				if not parseError:
+					newsong = SongDetails(
+						url=songURL,name=title,views=views,
+						dislikes=dislikes,likes=likes
+						)
+					newsong.save()
+					suggestion = SuggestedSongs(suggestedTo=suggestedTo,
+						suggestedBy=suggestedBy,song=newsong,isSeen=False)
+					suggestion.save()
+
+					alertMsg 	= mark_safe('''Your song is successfully suggested to %s. 
+								What a %s suggestion''' % (
+									suggestedTo.username,
+									'<i class="glyphicon glyphicon-heart"></i>'
+									))
+					alrtBstrpCls = 'alert-info'
+				else:
+					alertMsg	 = '''We are facing some error in retriving  
+									media info. Can you please try again later'''
+					alrtBstrpCls  = 'alert-danger'
+
+		# return render(request,self.template_name,{
+		# 	'form'			: formObj,
+		# 	'alrtBstrpCls'	: alrtBstrpCls,
+		# 	'alertMsg'		: alertMsg
+
+		# 	})
+		d = {
+			'alrtBstrpCls'	: alrtBstrpCls,
+			'alertMsg'		: alertMsg
+		}
+		return HttpResponse(json.dumps(d))
+
+
 
 class SuggestionList(ListView):
 
@@ -92,6 +171,8 @@ class SuggestionList(ListView):
 	def post(self,request,*args,**kwargs):
 
 		suggestionList = None
+		# c = {}
+		# c.update(csrf(request))
 		points_key = {
 			"cantsay"		: 5,
 			"abitlike"		: 25,
@@ -114,13 +195,16 @@ class SuggestionList(ListView):
 			suggestedBy = suggestedSong.suggestedBy
 			# suggestedSong.update(isSeen=True)
 			# suggestedSong.update(score=points)
-			print suggestedBy.id,suggestedBy.username
+			# print suggestedBy.id,suggestedBy.username
 			msg = 'You have given %s points to %s' % (points,suggestedBy.username)
-			print msg
+			# print msg
 			suggestionList = SuggestedSongs.objects.filter(suggestedTo=suggestedBy.id,isSeen=False)
-			print '******',suggestionList
 
-		return HttpResponseRedirect('/suggest')
+		return render(request,'suggestAjax.html',{
+			'suggestions' 		: suggestionList
+			})
+
+		# return HttpResponseRedirect('/suggest')
 
 class MyPlayList(ListView):
 	template_name = 'myplaylist.html'
@@ -138,33 +222,33 @@ class MyPlayList(ListView):
 			})
 
 
-class UpdateSongScoreAjax(ListView):
+# class UpdateSongScoreAjax(ListView):
 
-	def get(self,request,*args,**kwargs):
+# 	def get(self,request,*args,**kwargs):
 
-		d={}
+# 		d={}
 
-		points_key = {
-			"cantsay"		: 5,
-			"abitlike"		: 25,
-			"superawesome"	: 50,
-			"ok"		:	15,
-			"bad"		:	-10,
-			"what"		:	-50,
-		}
+# 		points_key = {
+# 			"cantsay"		: 5,
+# 			"abitlike"		: 25,
+# 			"superawesome"	: 50,
+# 			"ok"		:	15,
+# 			"bad"		:	-10,
+# 			"what"		:	-50,
+# 		}
 
-		suggestionId = self.kwargs["suggestionId"]
-		scoreString  = self.kwargs["scoreString"]
-		points = points_key[scoreString]
-		print suggestionId,scoreString,points
+# 		suggestionId = self.kwargs["suggestionId"]
+# 		scoreString  = self.kwargs["scoreString"]
+# 		points = points_key[scoreString]
+# 		print suggestionId,scoreString,points
 
-		suggestedSong = SuggestedSongs.objects.filter(id=suggestionId)[0]
-		suggestedBy = suggestedSong.suggestedBy
-		print suggestedBy.id,suggestedBy.username
-		msg = 'You have given %s points to %s' % (points,suggestedBy.username)
-		print msg
+# 		suggestedSong = SuggestedSongs.objects.filter(id=suggestionId)[0]
+# 		suggestedBy = suggestedSong.suggestedBy
+# 		print suggestedBy.id,suggestedBy.username
+# 		msg = 'You have given %s points to %s' % (points,suggestedBy.username)
+# 		print msg
 
 
-		return HttpResponse(json.dumps(msg))
+# 		return HttpResponse(json.dumps(msg))
 
 
--- gaanabhej/urls.py
+++ gaanabhej/urls.py
@@ -6,19 +6,19 @@ from django.views.decorators.csrf import csrf_exempt
 from django.contrib                 import admin
 from django.contrib.auth            import views as auth_views
 
-from playlist.views import SuggestASong,UpdateSongScoreAjax,SuggestionList,MyPlayList
+from playlist.views import SuggestASong,SuggestionList,MyPlayList
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'gaanabhej.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^$', SuggestASong.as_view(), name='play-list'),
-    url(r'^suggest$', SuggestASong.as_view(), name='play-list'),
+    # url(r'^$', SuggestASong.as_view(), name='play-list'),
+    url(r'^suggest', SuggestASong.as_view(), name='play-list'),
     url(r'^myplaylist$', MyPlayList.as_view(), name='play-list'),
-    url(r'^songs', csrf_exempt(SuggestionList.as_view()), name='play-list'),
-    url(r'^songs/(?P<suggestionId>[0-9]+)/(?P<scoreString>[-\w]+)/', csrf_exempt(SuggestionList.as_view()), name='play-list'),
+    url(r'^songs', SuggestionList.as_view(), name='play-list'),
+    url(r'^songs/(?P<suggestionId>[0-9]+)/(?P<scoreString>[-\w]+)/', SuggestionList.as_view(), name='play-list'),
 
-    url(r'^ajax/score/(?P<suggestionId>[0-9]+)/(?P<scoreString>[-\w]+)/$',UpdateSongScoreAjax.as_view(),name="update-score-ajax-view"),
+    # url(r'^ajax/score/(?P<suggestionId>[0-9]+)/(?P<scoreString>[-\w]+)/$',UpdateSongScoreAjax.as_view(),name="update-score-ajax-view"),
 
     url(r'^accounts/login/$',auth_views.login),
     url(r'^accounts/logout/$',auth_views.logout),
